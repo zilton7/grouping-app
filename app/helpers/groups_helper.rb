87e1a5bf7e040a21a2a66f3b2bd929820d782b8d
Zil Norvilis <zilasino27@gmail.com>
2021-04-23 09:45:06 +0200

    Style All Groups page

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,9 @@
 module GroupsHelper
+  def insert_icon(obj, size="50")
+    if obj.icon
+      image_tag(obj.icon, size: size, alt: "#{obj.name} icon")
+    else
+      image_tag("https://via.placeholder.com/50", size: size, alt: "#{obj.name} icon")
+    end
+  end
 end
