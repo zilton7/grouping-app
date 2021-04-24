157569e5848dba9552b8931a94882aa66001f18e
Zil Norvilis <zilasino27@gmail.com>
2021-04-24 17:20:32 +0200

    Add tests for Gift model

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -2,6 +2,6 @@ class Gift < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :group
 
-  validates :name, presence: true
-  validates :amount, presence: true
+  validates :name, presence: true, length: { minimum: 3, maximum: 24 }
+  validates :amount, presence: true, numericality: { only_float: true } 
 end
