5dc0fda4cf6ef1bd8c7de607e798cef016164821
Zil Norvilis <zilasino27@gmail.com>
2021-04-27 17:14:28 +0200

    Update Group to force icon url validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,5 @@ class Group < ApplicationRecord
   has_and_belongs_to_many :gifts
 
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 24 }
+  validates :icon, url: { allow_blank: true }
 end
