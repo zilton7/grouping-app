90bd5c22142806635306cc71535a574881f590cd
Zil Norvilis <zilasino27@gmail.com>
2021-04-28 12:04:38 +0200

    Move all logic from views to heleprs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def error_messages(obj)
+    if obj.errors.full_messages.any?
+      obj.errors.full_messages.each do |error_message|
+          return content_tag :p, error_message, class: 'text-danger text-center' if obj.errors.full_messages.first == error_message
+      end
+    end
+  end
 end
