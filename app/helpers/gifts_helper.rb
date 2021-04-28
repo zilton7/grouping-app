25b455f6eb79ae20c675b0bd84d952a3e0f66779
Zil Norvilis <zilasino27@gmail.com>
2021-04-28 11:29:58 +0200

    Move groups/gifts index logic to helpers

--- app/helpers/gifts_helper.rb
+++ app/helpers/gifts_helper.rb
@@ -18,4 +18,25 @@ module GiftsHelper
       image_tag('https://via.placeholder.com/50', size: size, alt: 'un-groupped gift icon')
     end
   end
+
+  def gifts_html_template(obj)
+    "<a href='/gifts/#{ obj.id }'>
+    <div class='list-item'>
+    #{ insert_gift_icon(obj) }
+        <div class='list-text'>
+            <p class='list-name'>#{ obj.name }</p>
+            <p class='list-date'>#{ obj.created_at }</p>
+        </div>
+        <p class='price'>Price: $#{ obj.amount }</p>
+    </div>
+    </a>"
+  end
+
+  def generate_gifts(gifts)
+    html_values = ""
+    gifts.each do |g|
+      html_values << gifts_html_template(g)
+    end
+    html_values.html_safe
+  end
 end
